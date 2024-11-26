827ac8950f6500973930db78b0dc56edb7d47edc
pradyumn yadav <yadavkeshu2007@gmail.com>
2024-11-26 21:10:16 +0530

    succesfully set up the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import Dashboard from "./Pages/Dashboard";
+import Send from "./Pages/Send"
+import Home from "./Pages/Home";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/Signin" element={<Signin/>} />
+      <Route path="/Signup" element={<Signup/>} />
+      <Route path="/Dashboard" element={<Dashboard/>} />
+      <Route path="/Send" element={< Send/>} />
+      </Routes>
+      </BrowserRouter>
+
+    </>
   )
 }
 
